bfd01a635f4b7d8158d291033b0c819e5b43957d
mvfi0 <muhammad.vegard@ui.ac.id>
2025-09-17 14:09:46 +0700

    Tutorial 3 finish

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-vegard-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://muhammad-vegard-footballnews.pbp.cs.ui.ac.id/"
+    "https://muhammad-vegard-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
